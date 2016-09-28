8a8476ff65d8081285b1dfcf3111b1cca7cd7126
もえなつ <mua.updown@gmail.com>
2016-09-29 01:49:24 +0900

    0929
    具体的な数値を入れるとDBに登録されるが変数を入れると動かない

--- form_2.php
+++ form_2.php
@@ -12,12 +12,14 @@
 		$db = new SQLite3('/Users/tunattu/test_db.db');
 		$your_name=$_POST['your_name'];
 		$all_data= $_POST['all_data'];
-		
-		$all_data=$_GET['all_data'];
-		$date = date('Y/m/d H:i:s');
-		$db->query("INSERT INTO test VALUES (" . $your_name . "," . $all_data . " ;)");
-		}
 
+		$date = date('Y/m/d H:i:s');
+		//$db->exec("CREATE TABLE info(name text, own_text text)");
+		//$db->exec("INSERT INTO info (name, own_text) VALUES (".$your_name.",".$all_data.")");
+		
+		//変数どうすればいいかわからないのおおおおお
+		//$db->query("INSERT INTO info (name, own_text) VALUES ("'.$your_name'","'.$all_data.'")");
+	
 		?>
 
 		<h1>内容が送信されました</h1>

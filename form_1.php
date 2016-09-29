c068baa7cae0e9eb82725494fd36a49eaaa12b35
もえなつ <mua.updown@gmail.com>
2016-09-29 23:31:06 +0900

    0929
    DBに名前とテキストを書き込み

--- form_1.php
+++ form_1.php
@@ -10,8 +10,12 @@
 	<div id="mainform">
 		<h1>確認画面</h1>
 		<?php
+		session_start();
 		$your_name=$_POST['your_name'];
 		$comment = $_POST['comment'];
+
+		//$_SESSION['your_name']= $_POST['your_name'];
+		//$_SESSION['comment']= $_POST['comment'];
 		$comment = nl2br($comment);
 
 		$data = "<hr>\r\n";
@@ -21,11 +25,14 @@
 
 		echo $data;
 		echo "<br>" . "以上の内容で間違いないですか？"
+
 		?>
 
 		<form action="form_2.php" method="POST">
-		<input type="hidden" name="all_data" value="<?php echo $data; ?>">
-		<input type="submit" value="送信">
+			<input type="hidden" name="all_data" value="<?php echo $data; ?>">
+			<input type="hidden" name="your_name" value="<?php echo $your_name; ?>">
+			<input type="hidden" name="comment" value="<?php echo $comment; ?>">
+			<input type="submit" value="送信">
 		</form>
 
 	</div>
--- form_2.php
+++ form_2.php
@@ -9,9 +9,11 @@
 <body>
 	<div id="mainform">
 		<?php
+		session_start();
 		$db = new SQLite3('/Users/tunattu/test_db.db');
-		$your_name=$_POST['your_name'];
 		$all_data= $_POST['all_data'];
+		$your_name=$_POST['your_name'];
+		$comment = $_POST['comment'];
 
 		$date = date('Y/m/d H:i:s');
 		//$db->exec("CREATE TABLE info(name text, own_text text)");
@@ -19,12 +21,21 @@
 		
 		//変数どうすればいいかわからないのおおおおお
 		//$db->query("INSERT INTO info (name, own_text) VALUES ("'.$your_name.'","'.$all_data.'")");
+
+		$stmt = $db->prepare("INSERT INTO info(name, own_text) VALUES(?, ?)");
+		$stmt->bindParam(1, $your_name);
+		$stmt->bindParam(2, $comment);
+		$stmt->execute();
+
+		//echo $_SESSION["your_name"];
+		//echo $_SESSION["comment"];
 		?>
 
 		<h1>内容が送信されました</h1>
 		<?php
 		echo $date;
 		echo $all_data;
+		
 		?>
 	</div>
 </body>

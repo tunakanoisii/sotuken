8d585120bec55762ef5b501e2fcddc869da344d3
もえなつ <mua.updown@gmail.com>
2016-10-05 15:43:54 +0900

    1005
    テスト

--- index.php
+++ index.php
@@ -6,11 +6,10 @@
 	<title>フォームからデータを受け取る</title>
 </head>
 <body>
-<div id="infos">
-<<<<<<< HEAD
-<div id="form"><p>入力フォームは<a href="form_top.html">こちら</p></a></div>
-	<?php
-	$db = new SQLite3('/Users/tunattu/test_db.db');
+	<div id="infos">
+		<div id="form"><p>入力フォームは<a href="form_top.html">こちら</p></a></div>
+		<?php
+		$db = new SQLite3('/Users/tunattu/test_db.db');
 
 	$results = $db->query('SELECT * FROM info order by date desc');//DBのデータを降順にする
 
@@ -19,24 +18,21 @@
 		echo '<p>' . $data[2] . '</p><br>';
 		echo '<p>[名前]' . $data[0] . '</p>';
 		echo '<p>[内容]<br>' . $data[1] . '</p>';
-=======
+		
+		<?php
+		$db = new SQLite3('/Users/tunattu/test_db.db');
 
-	<?php
-	$db = new SQLite3('/Users/tunattu/test_db.db');
+		$results = $db->query('SELECT * FROM info');
 
->>>>>>> master
-	$results = $db->query('SELECT * FROM info');
-
-	while($data = $results->fetchArray()){
-		echo '<div class="own_info">';
-		echo '<p>名前:' . $data[0] . '</p>';
-		echo '<p>内容:' . $data[1] . '</p>';
-		echo '</div>';
-	}
-
+		while($data = $results->fetchArray()){
+			echo '<div class="own_info">';
+			echo '<p>名前:' . $data[0] . '</p>';
+			echo '<p>内容:' . $data[1] . '</p>';
+			echo '</div>';
+		}
 		$db->close();
-	?>
-	
+		?>
+
 	</div>
 
 </body>

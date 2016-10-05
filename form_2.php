f0a3c2ad8e580d054eb3d32e741f89a5a27c3f14
もえなつ <mua.updown@gmail.com>
2016-10-05 15:30:00 +0900

    Merge branch 'master' into form

--- form_2.php
+++ form_2.php
@@ -39,6 +39,7 @@
 		?>
 
 		<br><p>他にも内容を追加しますか？</p>
+		<p>他にも内容を追加しますか？</p>
 		<form action="form_top.html" method="post">
 			<input type ="submit" value="続けて入力する">
 		</form>
--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 <body>
 <div id="infos">
 <div id="form"><p>入力フォームは<a href="form_top.html">こちら</p></a></div>
-
 	<?php
 	$db = new SQLite3('/Users/tunattu/test_db.db');
 
@@ -19,6 +18,12 @@
 		echo '<p>' . $data[2] . '</p><br>';
 		echo '<p>[名前]' . $data[0] . '</p>';
 		echo '<p>[内容]<br>' . $data[1] . '</p>';
+	$results = $db->query('SELECT * FROM info');
+
+	while($data = $results->fetchArray()){
+		echo '<div class="own_info">';
+		echo '<p>名前:' . $data[0] . '</p>';
+		echo '<p>内容:' . $data[1] . '</p>';
 		echo '</div>';
 	}
 

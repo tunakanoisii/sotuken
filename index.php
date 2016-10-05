44b03eb5b56b2ff8b7db360f5be44c00c5e7038e
もえなつ <mua.updown@gmail.com>
2016-10-05 15:47:07 +0900

    1005
    コンフリクトがおおい

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<title>フォームからデータを受け取る</title>
 </head>
 <body>
+<<<<<<< Updated upstream
 	<div id="infos">
 		<div id="form"><p>入力フォームは<a href="form_top.html">こちら</p></a></div>
 		<?php
@@ -18,17 +19,7 @@
 		echo '<p>' . $data[2] . '</p><br>';
 		echo '<p>[名前]' . $data[0] . '</p>';
 		echo '<p>[内容]<br>' . $data[1] . '</p>';
-		
-		<?php
-		$db = new SQLite3('/Users/tunattu/test_db.db');
 
-		$results = $db->query('SELECT * FROM info');
-
-		while($data = $results->fetchArray()){
-			echo '<div class="own_info">';
-			echo '<p>名前:' . $data[0] . '</p>';
-			echo '<p>内容:' . $data[1] . '</p>';
-			echo '</div>';
 		}
 		$db->close();
 		?>
